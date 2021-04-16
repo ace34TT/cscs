db5189af0f28a9cb0e791cad521de986ad461ec2
user1251 <tafinasoa35@gmail.com>
2021-04-16 19:45:12 +0200

    home

--- index.php
+++ index.php
@@ -10,11 +10,28 @@
 </head>
 
 <body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2 col-md-12 col-sm-12">
+                    <img src="Assets/Images/Logo.png" height="100px" width="100px" alt="" />
+
+                </div>
+            </div>
+        </div>
+    </header>
     <div class="container">
+        <div class="row text-center">
+            <button type="button" style="height: 50px; width : 400px" class="btn btn-primary btn-lg">Large button</button>
+        </div>
         <div class="row">
-            <button disabled="disabled"></button>
+            <br> <br>
+        </div>
+        <div class="row text-center">
+            <button type="button" style="height: 50px; width : 400px" class="btn btn-primary btn-lg">Large button</button>
         </div>
     </div>
+    <footer></footer>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 
